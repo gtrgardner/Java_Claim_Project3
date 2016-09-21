da31d4908761feaa35b204819ad91d8fdc16b35d
Robert Gardner <gtrgardner@gmail.com>
2016-09-21 18:52:06 -0500

    added db connection wallet class

--- src/survivalstoremain/Wallet.java
+++ src/survivalstoremain/Wallet.java
@@ -10,22 +10,14 @@ import java.sql.SQLException;
 //import java.util.Scanner;
 
 public class Wallet{
-	 //public List<Products> prod = new ArrayList<Products>();
+	 
 	SurvivalDAOImpl impl = new SurvivalDAOImpl();
-	private String walletTotal;
-
+	
 	public  void WalletInter() throws SQLException {
 		String sql = null;
 		ResultSet rs = null;
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-			} catch (Exception ex) {
-
-			}
-		 Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"
-					+ "products?autoReconnect=true&useSSL=false",
-					"robert", "robert");
-		
+		Connection conn = impl.getConnection();
+			
 		 sql = ("SELECT wallet_total FROM customer");
 			PreparedStatement statement = conn.prepareStatement(sql);
 			rs = statement.executeQuery();
