dd2d755923c7944b8961347d46d84880b434b1a0
Robert Gardner <gtrgardner@gmail.com>
2016-09-25 21:59:56 -0500

    no change

--- src/survivalstoremain/Wallet.java
+++ src/survivalstoremain/Wallet.java
@@ -9,26 +9,24 @@ import java.sql.SQLException;
 //import java.util.List;
 //import java.util.Scanner;
 
-public class Wallet{
-	 
+public class Wallet {
+
 	SurvivalDAOImpl impl = new SurvivalDAOImpl();
-	
-	public  void WalletInter() throws SQLException {
+
+	public void WalletInter() throws SQLException {
 		String sql = null;
 		ResultSet rs = null;
 		Connection conn = impl.getConnection();
-			
-		 sql = ("SELECT wallet_total FROM customer");
-			PreparedStatement statement = conn.prepareStatement(sql);
-			rs = statement.executeQuery();
-		while (rs.next()){
-			
+
+		sql = ("SELECT wallet_total FROM customer");
+		PreparedStatement statement = conn.prepareStatement(sql);
+		rs = statement.executeQuery();
+		while (rs.next()) {
+
 			System.out.println(rs.getString("wallet_total"));
 		}
 		conn.close();
 
 	}
-		
-		
-	}		
 
+}
